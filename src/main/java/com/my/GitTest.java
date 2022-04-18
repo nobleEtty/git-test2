c8ac4c90e2e4fb13b529505ded2261ecf9be2987
zkh <zkh@163.com>
2022-04-18 21:01:05 +0800

    hit-fix commit2

--- src/main/java/com/my/GitTest.java
+++ src/main/java/com/my/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello Git 2 !!!");
         System.out.println("hello Git 3 !!!");
         System.out.println("hello Git 4 !!!");
+        System.out.println("hello Git hot-fix !!!");
     }
 }
